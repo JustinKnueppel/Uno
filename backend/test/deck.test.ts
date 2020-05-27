fd9cc558c241d0b851719230173209911d628ed6
Justin <justinknueppel@gmail.com>
2020-05-27 17:52:42 -0400

    Assert that size decreases when card dealt

--- backend/test/deck.test.ts
+++ backend/test/deck.test.ts
@@ -1,23 +1,29 @@
-import { expect } from "chai"
-import Deck from "../src/deck"
+import { expect } from "chai";
+import Deck from "../src/deck";
 
-describe('Deck', () => {
+describe("Deck", () => {
   it("Class exists", () => {
-    expect(Deck).is.not.undefined
-  })
+    expect(Deck).is.not.undefined;
+  });
 
   it("Can be instantiated", () => {
-    expect(new Deck()).is.not.null
-  })
+    expect(new Deck()).is.not.null;
+  });
 
   it("Starts with 108 cards", () => {
     const deck: Deck = new Deck();
     expect(deck.size()).is.not.null;
-    expect(deck.size()).to.equal(108)
-  })
+    expect(deck.size()).to.equal(108);
+  });
 
   it("Deals a card", () => {
     const deck: Deck = new Deck();
-    expect(deck.deal()).to.be.a("Card")
-  })
-})
+    expect(deck.deal()).to.be.a("Card");
+  });
+
+  it("Size decreases when card dealt", () => {
+    const deck: Deck = new Deck();
+    deck.deal();
+    expect(deck.size()).to.equal(107);
+  });
+});
