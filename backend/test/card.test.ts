d4c09f6af028ae8d71a6edf83a00d06f71192a57
Justin <justinknueppel@gmail.com>
2020-05-27 15:40:23 -0400

    Change import style in test suite

--- backend/test/card.test.ts
+++ backend/test/card.test.ts
@@ -1,21 +1,20 @@
 import { expect } from "chai";
-import * as Card from "../src/card";
+import { Card, Type, Color } from "../src/card";
 
 describe("Card", () => {
   it("Card class exists", () => {
-    expect(Card.Card).is.not.undefined;
+    expect(Card).is.not.undefined;
   });
 
   it("Type enum exists", () => {
-    expect(Card.Type).is.not.undefined;
+    expect(Type).is.not.undefined;
   });
 
   it("Color enum exists", () => {
-    expect(Card.Color).is.not.undefined;
+    expect(Color).is.not.undefined;
   });
 
   it("Card can be instantiated", () => {
-    expect(new Card.Card(Card.Type.ZERO, Card.Color.RED)).is.not.null;
+    expect(new Card(Type.ZERO, Color.RED)).is.not.null;
   });
-
 })
