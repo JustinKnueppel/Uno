696b9795b87c107e8626f1fe041325b02f0fcf40
Justin <justinknueppel@gmail.com>
2020-05-27 18:27:37 -0400

    cant play non-wild of different number and color

--- backend/test/helper.test.ts
+++ backend/test/helper.test.ts
@@ -19,6 +19,7 @@ describe("Helper functions", () => {
       const secondZero = new Card(Type.ZERO, Color.RED);
       expect(Helpers.canPlayOn(firstZero, secondZero)).to.be.true;
     });
+
     it("Play wild on any color", () => {
       const wild = new Card(Type.WILD, Color.WILD)
       const blue = new Card(Type.ZERO, Color.BLUE);
@@ -29,6 +30,12 @@ describe("Helper functions", () => {
       expect(Helpers.canPlayOn(wild, red)).to.be.true
       expect(Helpers.canPlayOn(wild, yellow)).to.be.true
       expect(Helpers.canPlayOn(wild, green)).to.be.true
+    })
+
+    it("Cannot play non-wild card of different number and color", () => {
+      const firstCard = new Card(Type.ZERO, Color.BLUE)
+      const secondCard = new Card(Type.ONE, Color.RED)
+      expect(Helpers.canPlayOn(firstCard, secondCard)).to.be.false
     })
   });
 });
