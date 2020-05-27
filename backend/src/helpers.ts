664550ed0c0218ea9bec2e36fd79765242513a09
Justin <justinknueppel@gmail.com>
2020-05-27 18:21:11 -0400

    play same type of card on different color

--- backend/src/helpers.ts
+++ backend/src/helpers.ts
@@ -4,6 +4,9 @@ const canPlayOn = (nextCard: Card, currentCard: Card): boolean => {
   if (nextCard.color === currentCard.color) {
     return true;
   }
+  if (nextCard.type === currentCard.type) {
+    return true;
+  }
   return false;
 };
 
--- backend/test/helper.test.ts
+++ backend/test/helper.test.ts
@@ -1,19 +1,23 @@
-import { expect } from "chai"
-import * as Helpers from "../src/helpers"
-import { Card, Type, Color } from "../src/card"
+import { expect } from "chai";
+import * as Helpers from "../src/helpers";
+import { Card, Type, Color } from "../src/card";
 
 describe("Helper functions", () => {
-  describe('canPlayOn', () => {
+  describe("canPlayOn", () => {
     it("Has can play on function", () => {
-      expect(Helpers.canPlayOn).is.not.undefined
-    })
-  
+      expect(Helpers.canPlayOn).is.not.undefined;
+    });
+
     it("Play different number on same color", () => {
-      const firstBlue: Card = new Card(Type.ZERO, Color.BLUE)
-      const secondBlue: Card = new Card(Type.EIGHT, Color.BLUE)
-      expect(Helpers.canPlayOn(firstBlue, secondBlue)).to.be.true
-    })
-    
-  })
-  
-})
+      const firstBlue: Card = new Card(Type.ZERO, Color.BLUE);
+      const secondBlue: Card = new Card(Type.EIGHT, Color.BLUE);
+      expect(Helpers.canPlayOn(firstBlue, secondBlue)).to.be.true;
+    });
+
+    it("Play same type on different color", () => {
+      const firstZero = new Card(Type.ZERO, Color.BLUE);
+      const secondZero = new Card(Type.ZERO, Color.RED);
+      expect(Helpers.canPlayOn(firstZero, secondZero)).to.be.true;
+    });
+  });
+});
